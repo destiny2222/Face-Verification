ee7b3093156bd93fbdb45b3d5fd6e08251f4f1c2
destiny2222 <oghenekumeerieme@gmail.com>
2024-01-15 16:01:16 +0100

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,41 +7,6 @@ use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
-    public function veriff(Request $request)
-    {
-
-        // $sharedSecretKey = 'abcdef12-abcd-abcd-abcd-abcdef012345';
-        // $payload = '{"verification":{"callback":"https://veriff.com","person":{"firstName":"John","lastName":"Smith"},"document":{"type":"PASSPORT","country":"EE"},"vendorData":"unique id of the end-user","timestamp":"2016-05-19T08:30:25.597Z"}}';
-
-        // $signature = hash_hmac('sha256', $payload, $sharedSecretKey);
-        // $signature = strtolower($signature);
-
-        // echo "X-HMAC-SIGNATURE: " . $signature . PHP_EOL;
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'X-AUTH-CLIENT' => env('PUBLICABLE_KEY'),
-            'X-HMAC-SIGNATURE' => env('X-HMAC-SIGNATURE'),
-        ])->post(env('Base_Url') . '/v1/sessions/', [
-            "verification" => [
-                "callback" => "https://veriff.me",
-                "person" => [
-                    "firstName" => $request->input('firstName'),
-                    "lastName" => $request->input('lastName'),
-                    "idNumber" => $request->input('idNumber'),
-                ],
-                "document" => [
-                    "number" => "B01234567",
-                    "type" => "PASSPORT",
-                    "country" => "EE",
-                ],
-                "address" => [
-                    "fullAddress" => "Lorem Ipsum 30, 13612 Tallinn, Estonia",
-                ],
-                "vendorData" => "11111111",
-            ],
-        ]);
-    }
 
     private function generateHmacSignature($payload, $sharedSecretKey)
     {
